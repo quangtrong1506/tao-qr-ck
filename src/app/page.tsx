8c458fadf34b16620b9a996eb4ff8ca1768b2394
Quang Trong <quangtrong1506@gmail.com>
2024-05-12 23:24:12 +0700

    fix ux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { stringToSlug, to_vietnam_dong } from '@/helper/helpers';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import Loading from './Components/Loading';
 
 export default function Home() {
     const router = useRouter();
@@ -100,7 +99,7 @@ export default function Home() {
                         type="submit"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
-                        {isLoading ? <Loading /> : <>Tạo QR</>}
+                        {isLoading ? <span>Loading...</span> : <>Tạo QR</>}
                     </button>
                 </form>
             </main>
