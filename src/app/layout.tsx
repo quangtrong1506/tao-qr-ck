a1ea70ee6e593a4afdc0661387d93c03b80b871d
Quang Trong <quangtrong1506@gmail.com>
2024-05-13 13:35:20 +0700

    fix language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="vi">
             <head>
                 <link rel="shortcut icon" href="/images/icon.icon" type="image/x-icon" />
             </head>
