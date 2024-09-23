bbdc9eff6408c29eba4ac5af8d84bf18b860400e
Quang Trong <80852103+quangtrong1506@users.noreply.github.com>
2024-09-23 17:42:03 +0700

    fix bug description

--- src/app/qr-code/page.tsx
+++ src/app/qr-code/page.tsx
@@ -1,8 +1,7 @@
-import { Suspense } from 'react';
-
-import { LIST_BANK, MY_BANKS } from '@/helper/const';
+import { MY_BANKS } from '@/helper/const';
 import { to_vietnam_dong } from '@/helper/helpers';
 import type { Metadata, ResolvingMetadata } from 'next';
+import { Suspense } from 'react';
 import { QRCode } from '../Components/qr-code';
 
 type Props = {
@@ -11,67 +10,46 @@ type Props = {
 };
 
 export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
-    // read route params
-    // const id = params.id;
-    console.log(params, searchParams);
-
-    // // fetch data
-    const money = searchParams.money;
-    const content = searchParams.content;
-    const bank = searchParams.bank;
-    // document.title = `QRCode ${money} đ`;
+    const { money, content, bank } = searchParams;
     const myBank = MY_BANKS.find((item) => item.id === bank);
-    if (!myBank)
+
+    if (!myBank) {
+        return { title: '404', description: 'Page not found' };
+    }
+
+    try {
+        const previousImages = ['/images/qr-code.png'];
+        const title = `QR thanh toán tiền cho Quang Trọng`;
+        let descriptionContent = 'QR thanh toán';
+
+        if (money) {
+            const formattedMoney = to_vietnam_dong(money as string);
+            descriptionContent += ` ${formattedMoney}₫ bằng chữ ${formattedMoney}`;
+        } else {
+            descriptionContent += ' hoá đơn';
+        }
+
+        if (content) {
+            descriptionContent += `, Nội dung chuyển khoản ${content}`;
+        }
+
         return {
-            title: '404',
-            description: 'Page not found',
+            title,
+            description: descriptionContent,
+            openGraph: {
+                images: ['/some-specific-page-image.jpg', ...previousImages],
+            },
         };
-    const BANK = LIST_BANK.find((item) => item.id === myBank.bank_id);
-    const myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-    const raw = JSON.stringify({
-        acqId: BANK?.bin,
-        accountName: 'LUONG QUANG TRONG',
-        accountNo: myBank.value,
-        amount: money,
-        addInfo: content,
-        template: 'qr_only',
-    });
-
-    const data = await fetch('https://api.vietqr.io/v2/generate', {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow',
-    }).then((response) => response.json());
-    if (data.code != '00')
-        return {
-            title: '400',
-            description: data.desc,
-        };
-    const previousImages = data.data.qrDataURL || [];
-
-    const title = `QR thanh toán tiền cho Quang Trọng`;
-    let contentString = 'QR thanh toán';
-    if (money) contentString += ` ${to_vietnam_dong(money as string)}₫ bằng chữ ${to_vietnam_dong(money as string)}`;
-    else contentString += ' hoá đơn';
-    if (content) contentString += `, Nội dung chuyển khoản ${content}`;
-
-    return {
-        title,
-        description: contentString || '',
-        openGraph: {
-            images: ['/some-specific-page-image.jpg', ...previousImages],
-        },
-    };
+    } catch (error) {
+        console.error('Error fetching QR code data:', error);
+        return { title: '500', description: 'Internal Server Error' };
+    }
 }
 
-const QRCodePage = () => {
-    return (
-        <Suspense>
-            <QRCode />
-        </Suspense>
-    );
-};
+const QRCodePage = () => (
+    <Suspense fallback={<div>Loading...</div>}>
+        <QRCode />
+    </Suspense>
+);
 
 export default QRCodePage;
