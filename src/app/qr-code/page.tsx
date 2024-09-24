418efb9220c7caab15ed7e7c5a67b1a49e0d73eb
Quang Trong <80852103+quangtrong1506@users.noreply.github.com>
2024-09-24 10:07:28 +0700

    test -1

--- src/app/qr-code/page.tsx
+++ src/app/qr-code/page.tsx
@@ -1,6 +1,4 @@
-import { MY_BANKS } from '@/helper/const';
-import { to_vietnam_dong } from '@/helper/helpers';
-import type { Metadata, ResolvingMetadata } from 'next';
+import type { Metadata } from 'next';
 import { Suspense } from 'react';
 import { QRCode } from '../Components/qr-code';
 
@@ -11,41 +9,41 @@ type Props = {
 export const metadata: Metadata = {
     icons: '/images/qr-code.ico',
 };
-export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
-    const { m, c, b } = searchParams;
-    const myBank = MY_BANKS.find((item) => item.id === b);
+// export async function generateMetadata({ params, searchParams }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+//     const { m, c, b } = searchParams;
+//     const myBank = MY_BANKS.find((item) => item.id === b);
 
-    if (!myBank) {
-        return { title: '404', description: 'Page not found' };
-    }
+//     if (!myBank) {
+//         return { title: '404', description: 'Page not found' };
+//     }
 
-    try {
-        const title = `QR thanh toán tiền cho Quang Trọng`;
-        let descriptionContent = 'QR thanh toán';
+//     try {
+//         const title = `QR thanh toán tiền cho Quang Trọng`;
+//         let descriptionContent = 'QR thanh toán';
 
-        if (m) {
-            const formattedMoney = to_vietnam_dong(m as string);
-            descriptionContent += ` ${parseInt(m as string, 10).toLocaleString('vi-VN')}₫ bằng chữ ${formattedMoney}`;
-        } else {
-            descriptionContent += ' hoá đơn';
-        }
+//         if (m) {
+//             const formattedMoney = to_vietnam_dong(m as string);
+//             descriptionContent += ` ${parseInt(m as string, 10).toLocaleString('vi-VN')}₫ bằng chữ ${formattedMoney}`;
+//         } else {
+//             descriptionContent += ' hoá đơn';
+//         }
 
-        if (c) {
-            descriptionContent += `, Nội dung chuyển khoản: "${c}"`;
-        }
-        return {
-            title,
-            description: descriptionContent,
-            icons: ['/images/qr-code.ico', 'https://qr-nhantien.vercel.app/images/qr-code.ico'],
-            openGraph: {
-                images: ['/images/qr-code.ico', 'https://qr-nhantien.vercel.app/images/qr-code.ico'],
-            },
-        };
-    } catch (error) {
-        console.error('Error fetching QR code data:', error);
-        return { title: '500', description: 'Internal Server Error' };
-    }
-}
+//         if (c) {
+//             descriptionContent += `, Nội dung chuyển khoản: "${c}"`;
+//         }
+//         return {
+//             title,
+//             description: descriptionContent,
+//             icons: ['/images/qr-code.ico', 'https://qr-nhantien.vercel.app/images/qr-code.ico'],
+//             openGraph: {
+//                 images: ['/images/qr-code.ico', 'https://qr-nhantien.vercel.app/images/qr-code.ico'],
+//             },
+//         };
+//     } catch (error) {
+//         console.error('Error fetching QR code data:', error);
+//         return { title: '500', description: 'Internal Server Error' };
+//     }
+// }
 
 const QRCodePage = () => (
     <Suspense fallback={<div>Loading...</div>}>
